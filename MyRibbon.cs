0a7f986107e7cba1212b785df323f7809913b4a2
Govert van Drimmelen <govert@dnakode.com>
2024-02-27 12:29:15 +0200

    Refactor info tracked per workbook

--- MyRibbon.cs
+++ MyRibbon.cs
@@ -1,6 +1,10 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using ExcelDna.Integration;
 using ExcelDna.Integration.CustomUI;
+using ExcelDna.Integration.Extensibility;
 using Microsoft.Office.Interop.Excel;
 using Application = Microsoft.Office.Interop.Excel.Application;
 
@@ -9,8 +13,17 @@ namespace TestRibbonWorkbookTracking
     [ComVisible(true)]
     public class MyRibbon : ExcelRibbon
     {
+        class WorkbookInfo
+        {
+            public string Note;
+            public bool Flag;
+        }
+
+        Application _app;
         IRibbonUI _ribbon;
-        Workbook _activeWorkbook;
+        bool _shuttingDown;
+
+        Dictionary<Workbook, WorkbookInfo> _workbookInfo = new Dictionary<Workbook, WorkbookInfo>();
 
         public override string GetCustomUI(string RibbonID)
         {
@@ -23,31 +36,80 @@ namespace TestRibbonWorkbookTracking
             return RibbonResources.ResourceManager.GetObject(imageId);
         }
 
+        public override void OnConnection(object Application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom)
+        {
+            _app = (Application)Application;
+        }
+
+        public override void OnBeginShutdown(ref Array custom)
+        {
+            _shuttingDown = true;
+            // Excel seems to get unhappy with some events during shutdown
+            _app.WorkbookActivate -= Application_WorkbookActivate;
+            _app.WorkbookBeforeClose -= Application_WorkbookBeforeClose;
+        }
+
         public void OnLoad(IRibbonUI ribbon)
         {
             _ribbon = ribbon;
-            var app = (Application)ExcelDnaUtil.Application;
-            _activeWorkbook = app.ActiveWorkbook;
-            // Track workbook changes with inline lambda event handlers
-            app.WorkbookActivate += (wb) =>
-            {
-                _activeWorkbook = wb;
-                _ribbon.Invalidate();
-            };
-            app.SheetChange += (sh, target) =>
-            {
-                _ribbon.InvalidateControl("label2");
-            };
+            _app.WorkbookActivate += Application_WorkbookActivate;
+            _app.WorkbookBeforeClose += Application_WorkbookBeforeClose;
         }
 
-        public string GetActiveWorkbookLabel(IRibbonControl control)
+        void Application_WorkbookActivate(Workbook Wb)
         {
-            return _activeWorkbook?.Name ?? "<No workbook>";
+            if (!_workbookInfo.ContainsKey(Wb))
+                _workbookInfo.Add(Wb, new WorkbookInfo { Note = "<New>", Flag = false });
+
+            _ribbon.Invalidate(); // Ensures the callbacks are called after activate
+            Debug.Print("WorkbookActivate: " + Wb.Name);
         }
 
-        public string GetA1Label(IRibbonControl control)
+        private void Application_WorkbookBeforeClose(Workbook Wb, ref bool Cancel)
         {
-            return _activeWorkbook?.ActiveSheet?.Range["A1"].Value2?.ToString() ?? "<No A1>";
+            // NOTE: This is not the best way to track workbook closing, since user can cancel the close.
+            //       Tracking all open workbooks in Excel is hard - see e.g. https://stackoverflow.com/questions/58643395/how-to-detect-when-a-workbook-is-closing
+            _workbookInfo.Remove(Wb);
+        }
+
+        WorkbookInfo GetActiveInfo()
+        {
+            var wb = _app.ActiveWorkbook;
+            if (wb == null)
+                return null;
+            return _workbookInfo[wb];
+        }
+
+        public string GetWorkbookNote(IRibbonControl control)
+        {
+            var info = GetActiveInfo();
+            if (info == null)
+                return "<No Wb>";
+            return info.Note;
+        }
+
+        public void SetWorkbookNote(IRibbonControl control, string text)
+        {
+            var info = GetActiveInfo();
+            if (info == null) 
+                return;
+            info.Note = text;
+        }
+
+        public bool GetWorkbookFlag(IRibbonControl control)
+        {
+            var info = GetActiveInfo();
+            if (info == null) 
+                return false;
+            return info.Flag;
+        }
+
+        public void SetWorkbookFlag(IRibbonControl control, bool value)
+        {
+            var info = GetActiveInfo();
+            if (info == null) 
+                return;
+            info.Flag = value;
         }
 
         public void OnButtonPressed(IRibbonControl control)
